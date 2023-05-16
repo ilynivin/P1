fb5ed58247f7d28aff2b7eaa4affa6c289043f20
Nivins <nivinsvysakh2020@outlook.com>
2023-05-16 17:19:46 +0530

    Initial commit

---
+++ run.py
@@ -0,0 +1,18 @@
+import math
+
+
+print("\tWelcome to Big or small Finder! ")
+print("\n \t Please Star me On Github !")
+
+a = int(input("Enter Number A :"))
+b = int(input("Enter Number B :"))
+
+if (a>b):
+    print(a ,"Is bigger than" ,b)
+
+if (b>a):
+    print(b,"is Bigger than",a)
+
+
+if (a == b):
+    print("Both are Equal!")
